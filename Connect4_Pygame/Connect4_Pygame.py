9a183ceb8455ef952ed7118d2ccb8678e6b8aa22
Joles-Doe <105786625+Joles-Doe@users.noreply.github.com>
2022-07-06 11:48:55 +0100

    tidied up

--- Connect4_Pygame/Connect4_Pygame.py
+++ Connect4_Pygame/Connect4_Pygame.py
@@ -9,7 +9,7 @@ pygame.init()
 class gamecolours():
     global red, blue, lightblue, yellow, white, slate, black
     red = (255, 0, 0)
-    blue = 	(0, 0, 255)
+    blue = (0, 0, 255)
     lightblue = (111, 187, 211)
     yellow = (255, 255, 0)
     white = (255, 255, 255)
